460c1675de4de207986e6ad2f23b6eda5f5645e1
changyanwei <1926174536@qq.com>
2021-01-19 17:11:27 +0800

    设置路径别名，动态改变页面标题

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,50 @@ const Profile = () => import('../views/profile/Profile')
 
 const routes = [
   {
+    path:'/',
+    // component:Home
+    redirect:'/home'
+  },
+  {
     path:'/home',
-    component:Home
+    component:Home,
+    meta:{
+      title:'首页'
+    }
   },
   {
     path: '/category',
-    component:Category
+    component:Category,
+    meta:{
+      title:'分类'
+    }
   },
   {
     path:'/shop',
-    component:Shop
+    component:Shop,
+    meta:{
+      title:'购物车'
+    }
   },
   {
     path:'/profile',
-    component:Profile
+    component:Profile,
+    meta:{
+      title:'我的'
+    }
   }
 ]
 
 const router = new Router({
   routes,
   mode:'history'
+})
+
+router.beforeEach((to,from,next) => {
+  console.log(to);
+  // document.title = to.meta.title;
+  document.title = to.matched[0].meta.title;
+  next();
 })
 
 export default router
