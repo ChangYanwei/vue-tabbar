bee54b466d691106151fab9a293adb254d37ca7d
changyanwei <1926174536@qq.com>
2021-01-19 15:32:23 +0800

    被激活时改变图片和文字颜色

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <tab-bar>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/home.png">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.png">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/category.png">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category-active.png">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/shop.png">
+        <img slot="item-icon-active" src="./assets/img/tabbar/shop-active.png">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/profile.png">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile-active.png">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="tab-bar-item">
-    <slot name="item-icon"></slot>
-    <slot name="item-text"></slot>
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "TabBarItem"
+    name: "TabBarItem",
+    data() {
+      return {
+        isActive: true
+      }
+    }
   }
 </script>
 
@@ -22,5 +34,10 @@
   .tab-bar-item img {
     width: 24px;
     height: 24px;
+  }
+
+  /*设置文字颜色*/
+  .active {
+    color: orangered;
   }
 </style>
